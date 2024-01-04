0fc94801fe4bdbc017ce7937be8cfd0662c8e6bc
Vo Hoang <thoatkhoinicktao2002@gmail.com>
2024-01-04 13:25:44 +0700

    update

--- spark_streaming/main.py
+++ spark_streaming/main.py
@@ -1,10 +1,33 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
+from pyspark.sql.functions import from_json, col, udf
 from pyspark.sql.types import StructType, StructField, StringType
 import os
 
 # Import UDFs from preprocess.py
-import preprocess
+import re
+
+# Function to process HTML and extract text
+@udf(returnType=StringType())
+def process_html(content):
+    cleaned_text = re.sub(r"<.*?>", "", content)
+    cleaned_text = re.sub(r"\s+", " ", cleaned_text).strip()
+    return cleaned_text
+
+# Function to convert views, num_answer, votes to numeric values
+@udf(returnType=StringType())
+def convert_to_numeric(value):
+    try:
+        if value.endswith("k"):
+            return str(int(float(value[:-1]) * 1000))
+        elif value.endswith("m"):
+            return str(int(float(value[:-1]) * 1000000))
+        elif value.endswith("b"):
+            return str(int(float(value[:-1]) * 1000000000))
+        else:
+            return str(int(value))
+    except ValueError:
+        return "0"
+
 
 # Kafka settings
 kafka_topic_name = os.environ.get("KAFKA_TOPIC_NAME", "stack_exchange")
